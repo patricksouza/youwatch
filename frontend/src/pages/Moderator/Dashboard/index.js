9e81f82f228bb79e43f8234db2c3ba85d62247c4
patricksouza <patrickrsmiranda@gmail.com>
2021-01-11 17:32:42 -0400

    add novas rotas e pages

--- frontend/src/pages/Moderator/Dashboard/index.js
+++ frontend/src/pages/Moderator/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
 import api from '../../../services/api';
 
 export default function ModDashboard() {
@@ -6,7 +7,14 @@ export default function ModDashboard() {
     return (
         <>
            <div className="container">
-
+                <div className="row d-flex justify-content-between">
+                    <div className="col-md-2 mb-2">
+                        <Link to="/moderator/videos">Vídeos</Link>
+                    </div>
+                    <div className="col-md-2 mb-2">
+                        <Link to="/moderator/users">Usuários</Link>
+                    </div>
+                </div>
            </div>
         </>
     );
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 //Pages
 import Home from './pages/Home';
 import NewVideo from './pages/NewVideo';
+import ModVideos from './pages/Moderator/Videos';
 
 
 export default function Routes() {
@@ -12,6 +13,7 @@ export default function Routes() {
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/new" component={NewVideo} />
+                <Route path="/moderator/videos" component={ModVideos}/>
             </Switch>
         </BrowserRouter>
 
